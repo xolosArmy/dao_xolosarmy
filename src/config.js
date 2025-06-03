98bf92b36477cfc0c37ad874277e4ddfacabb0ab
xolosArmy Network <contacto@xolosramirez.com>
2025-06-03 15:08:40 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   chronikUrl: 'https://chronik.be.cash/xec',
-  daoAddress: 'ecash:qqxdaonetworkdummyaddressforvotes0',
-  tokenId: 'tokenid_placeholder_for_RMZ'
+  daoAddress: 'ecash:qqa4zjj0mt6gkm3uh6wcmxtzdr3p6f7cky4y7vujuw',
+  tokenId: '9e0a9d4720782cf661beaea6c5513f1972e0f3b1541ba4c83f4c87ef65f843dc'
 };
